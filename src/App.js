9cd7137451418e546a20c8eb580d70db9f3ce319
Brennen-Fisher <brennen_fisher@outlook.com>
2023-05-26 00:05:20 -0500

    mockup made

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-background">
+      <div className="nav"></div>
+      <div className="flexGroup">
+        <div className="side-btn">
+          <h1>test</h1>
+          <button>
+            
+          </button>
+          <h1>test</h1>
+        </div>
+        <div className="timeline">
+          <div className="tweet">
+            <p className="tweet-text">
+              this is some filler text woah dude thats pretty cool
+            </p>
+          </div>
+          <div className="tweet">
+            <p className="tweet-text">
+              this is some other filler text murder is ok
+            </p>
+          </div>
+          <div className="tweet">
+            <p className="tweet-text">
+              lorem ipsum dolor sit amet, consectetur adipiscing el aspect et
+              just e iure
+            </p>
+          </div>
+          <div className="tweet">
+            <p className="tweet-text">
+              domini tempor a ante dict tempor ullamcorper et just e iure
+            </p>
+          </div>
+          <div className="tweet">
+            <p className="tweet-text">
+              Rue doesn't make her bed what a fucking nerd lmao
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
